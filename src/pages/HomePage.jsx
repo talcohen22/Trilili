8cbfbec599803488441783de7c3dee7e21608c51
TamirKol <tamirk9596@gmail.com>
2023-10-03 23:26:55 +0300

    F/add-homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { utilService } from '../services/util.service.js'
+import { HomePageHeader } from './home-page-cmps/HomePageHeader.jsx'
 
 export function HomePage() {
     return (
-        <section>
-           <p>Lorem ipsum dolor sit amet.</p>
-           <button><Link to="/workspace">Start Demo</Link></button>
+        <section className='main-layout'>
+            <div className='homepage-container'>
+                <div className="homepage-header">
+                <HomePageHeader/>
+                </div>
+            <div class="homepage-content">
+                <div className="homepage-hero flex">
+                    <div>
+                        <h1>Trilili brings all your tasks, teammates, and tools together</h1>
+                        <p>Keep everything in the same place-even if your team isn’t.</p>
+                        <button><Link to="/workspace">Start Managing</Link></button>
+                    </div>
+                    <div>
+                        <img src="src//assets//img//home-img.webp" />
+                    </div>
+                </div>
+                {/* <div className="homepage-section"></div> */}
+
+            </div>
+            
+            </div>
         </section >
     )
 }
---
+++ src/pages/home-page-cmps/HomePageHeader.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+import { LogoApp } from "../../cmps/svg/ImgSvg"
+
+export function HomePageHeader() {
+
+    return (
+        <header className="app-header">
+            <nav className='flex justify-space-b align-center'>
+                <div className='btns-header-ops flex'>
+                    <div>
+                    <LogoApp/>
+                    <button className="btn-logo">Trilili</button>
+                    </div>
+                    <button className="btn-link">Features</button>
+                    <button className="btn-link">Solutions</button>
+                    <button className="btn-link">plans</button>
+                    <button className="btn-link">Pricing</button>
+                    <button className="btn-link">Resources</button>
+                </div>
+                <div>
+                    <button className="header-btn-start"><Link>Start Managing</Link></button>
+                </div>
+            </nav>
+        </header>
+    )
+}
