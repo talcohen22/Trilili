2ffba2fe86db7b57dc6c2f26dc6686004ebc4bdb
Stav Cohen <stavco28@gmail.com>
2023-10-03 21:09:22 +0300

    appHeader - change header navigite

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -6,66 +6,28 @@ import { login, logout, signup } from '../store/user.actions.js'
 import { LoginSignup } from './LoginSignup.jsx'
 
 export function AppHeader() {
-    // const user = useSelector(storeState => storeState.userModule.user)
-
-    // async function onLogin(credentials) {
-    //     try {
-    //         const user = await login(credentials)
-    //         showSuccessMsg(`Welcome: ${user.fullname}`)
-    //     } catch (err) {
-    //         showErrorMsg('Cannot login')
-    //     }
-    // }
-    // async function onSignup(credentials) {
-    //     try {
-    //         const user = await signup(credentials)
-    //         showSuccessMsg(`Welcome new user: ${user.fullname}`)
-    //     } catch (err) {
-    //         showErrorMsg('Cannot signup')
-    //     }
-    // }
-    // async function onLogout() {
-    //     try {
-    //         await logout()
-    //         showSuccessMsg(`Bye now`)
-    //     } catch (err) {
-    //         showErrorMsg('Cannot logout')
-    //     }
-    // }
 
     return (
         <header className="app-header " >
             <nav className='flex justify-space-b align-center'>
                 <div className='btns-header-ops'>
-                    <button className="btn-ops btn-logo">Trilili</button>
+                    <button className="btn-ops btn-logo">
+                        <Link to="/">
+                            Trilili
+                        </Link>
+                    </button>
                     <button className="btn-ops btn-workspaces">Workspaces</button>
                     <button className="btn-ops btn-more">More</button>
                     <button className="btn-ops btn-plus-options">More</button>
                 </div>
 
                 <div className='btns-header-user'>
-                    <input type="text" name="search-bar" id="search-bar" />
+                    <input type="text" name="search-bar" id="search-bar" placeholder='Search' />
                     <button className="btn-user btn-notifications">notifications</button>
-                    <button className="btn-user btn-img-user">user img</button>
+                    <button className="btn-user btn-img-user">user
+                        {/* <img src="https://friconix.com/png/fi-cnsuxl-user-circle.png" alt="" /> */}
+                    </button>
                 </div>
-
-                {/* {routes.map(route => <NavLink key={route.path} to={route.path}>{route.label}</NavLink>)}
-
-                {user &&
-                    <span className="user-info">
-                        <Link to={`user/${user._id}`}>
-                            {user.imgUrl && <img src={user.imgUrl} />}
-                            {user.fullname}
-                        </Link>
-                        <span className="score">{user.score?.toLocaleString()}</span>
-                        <button onClick={onLogout}>Logout</button>
-                    </span>
-                }
-                {!user &&
-                    <section className="user-info">
-                        <LoginSignup onLogin={onLogin} onSignup={onSignup} />
-                    </section>
-                } */}
             </nav>
         </header>
     )
