35024804434fd4ba6be575e1839fd1d8206e724c
Stav Cohen <stavco28@gmail.com>
2023-10-03 23:15:37 +0300

    add imgSvg component

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,24 +1,25 @@
 import { Link, NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import routes from '../routes'
-import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
-import { login, logout, signup } from '../store/user.actions.js'
-import { LoginSignup } from './LoginSignup.jsx'
+// import { useSelector } from 'react-redux'
+// import routes from '../routes'
+// import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+// import { login, logout, signup } from '../store/user.actions.js'
+// import { LoginSignup } from './LoginSignup.jsx'
+import { LogoApp } from './svg/ImgSvg'
 
 export function AppHeader() {
-
     return (
         <header className="app-header " >
             <nav className='flex justify-space-b align-center'>
                 <div className='btns-header-ops'>
                     <button className="btn-ops btn-logo">
+                        <LogoApp />
                         <Link to="/">
                             Trilili
                         </Link>
                     </button>
                     <button className="btn-ops btn-workspaces">Workspaces</button>
+                    <button className="btn-ops btn-recent">Recent</button>
                     <button className="btn-ops btn-more">More</button>
-                    <button className="btn-ops btn-plus-options">More</button>
                 </div>
 
                 <div className='btns-header-user'>
