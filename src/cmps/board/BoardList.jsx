7acdb0e159a6a16344177b0dd7b50a45f8d95997
Tal Cohen <talco4088@gmail.com>
2023-10-04 00:08:44 +0300

    working on boardIndex cmp

--- src/cmps/board/BoardList.jsx
+++ src/cmps/board/BoardList.jsx
@@ -1,16 +1,23 @@
+import { UserSvg } from "../svg/UserSvg";
 import { BoardPreview } from "./BoardPreview";
 
 export function BoardList({ boards }) {
     return (
-            <section className="board-list-container">
+        <section className="board-list-container">
+            <h1>WorkSpace name</h1>
+            <div className="boards-header">
+                <UserSvg/>
                 <h3>Your boards</h3>
-                <ul className="board-list">
-                    {boards.map(board =>
+            </div>
+            <ul className="board-list">
+                {boards.map(board =>
                     <li className="board-item" key={board._id}>
-                        <BoardPreview board={board} />
+                        <span className="board-item-content">
+                            <BoardPreview board={board} />
+                        </span>
                     </li>
                 )}
-                </ul>
-            </section >
+            </ul>
+        </section >
     )
 }
