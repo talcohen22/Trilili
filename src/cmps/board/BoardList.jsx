0d255bcd4a1b18e8c874b21fd201224718459466
Tal Cohen <talco4088@gmail.com>
2023-10-04 09:23:12 +0300

    added route  to BoardDetails cmp

--- src/cmps/board/BoardList.jsx
+++ src/cmps/board/BoardList.jsx
@@ -1,18 +1,26 @@
+import { useNavigate } from "react-router";
 import { UserSvg } from "../svg/ImgSvg";
 import { BoardPreview } from "./BoardPreview";
 
 export function BoardList({ boards }) {
+
+    const navigate = useNavigate()
+
+    function onGetDetails(boardId){
+        navigate(`board/${boardId}`)
+    }
+
     return (
         <section className="board-list-container">
             <h1>WorkSpace name</h1>
             <div className="boards-header">
-                <UserSvg/>
+                <UserSvg />
                 <h3>Your boards</h3>
             </div>
             <ul className="board-list">
                 {boards.map(board =>
-                    <li className="board-item" key={board._id}>
-                            <BoardPreview board={board} />
+                    <li className="board-item" onClick={() => onGetDetails(board._id)} key={board._id}>
+                        <BoardPreview board={board} />
                     </li>
                 )}
             </ul>
