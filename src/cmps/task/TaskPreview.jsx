a61e7b9c8a77e7ea371830e511417223f75bd046
Stav Cohen <stavco28@gmail.com>
2023-10-06 10:51:23 +0300

    start work on task preview modal

--- src/cmps/task/TaskPreview.jsx
+++ src/cmps/task/TaskPreview.jsx
@@ -3,10 +3,15 @@ import { PencilSvg } from "../svg/ImgSvg";
 
 export function TaskPreview({ task }) {
 
+    function inHandleClickEditTitle(taskId) {
+        console.log('taskId', taskId)
+    }
     return (
         <article className="task-card flex justify-space-b">
             <p className="task-title">{task.title}</p>
-            <button className="task-preview-btn flex justify-center align-center">
+            <button
+                className="task-preview-btn flex justify-center align-center"
+                onClick={() => inHandleClickEditTitle(task.id)}>
                 <PencilSvg />
             </button>
         </article>
