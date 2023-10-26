ccd544f62bd0e3cf857fd02814b883d5ef0f54c7
TamirKol <tamirk9596@gmail.com>
2023-10-26 23:26:13 +0300

    B/fix board filter css

--- src/cmps/board/BoardFilter.jsx
+++ src/cmps/board/BoardFilter.jsx
@@ -3,14 +3,14 @@ import { DashboardSvg, DotsSvg, FilterSvg, FullStarSvg, PowerUpSvg, ShareSvg, St
 import { boardService } from "../../services/board.service.local";
 
 export function BoardFilter({ board, onSetBoard }) {
-    const boardTitleLength= board.title.length
-    const [inputWidth, SetInputWidth] = useState(boardTitleLength*12)
+    const boardTitleLength = board.title.length
+    const [inputWidth, SetInputWidth] = useState(boardTitleLength * 9.5)
     const [boardTitle, setBoardTitle] = useState(board.title)
     const [isStarred, setIsStarred] = useState(board.isStarred)
     function handleIputLength(event) {
         const value = event.target.value
-        const updateBoard= board
-        updateBoard.title= value
+        const updateBoard = board
+        updateBoard.title = value
         onSetBoard(updateBoard)
         SetInputWidth(`${value.length * 10}px`)
         setBoardTitle(value)
@@ -27,7 +27,7 @@ export function BoardFilter({ board, onSetBoard }) {
             <header className="board-filter">
                 <section className="board-visibility">
                     <section className="header-title" style={{ width: inputWidth }} >
-                        <input type="text" onChange={handleIputLength} value={boardTitle}  style={{ width: inputWidth }}/>
+                        <input type="text" onChange={handleIputLength} value={boardTitle} style={{ width: inputWidth }} />
                     </section>
                     <section className=" board-filter">
                         <button onClick={handleIsStarred} className="board-filter-btn ">{isStarred ? <FullStarSvg /> : <StarSvg />}</button>
@@ -40,9 +40,12 @@ export function BoardFilter({ board, onSetBoard }) {
                     <button className="board-filter-btn"><PowerUpSvg /></button>
                     <button className="board-filter-btn filter"><FilterSvg /><span>Filter</span></button>
                     <section className="board-filter img">
-                        <span className="seperator"></span><img className="member-img" src="https://source.unsplash.com/random/300×300" alt="" />
-                        <img className="member-img" src="https://source.unsplash.com/random/350×350" alt="" />
-                        <img className="member-img" src="https://source.unsplash.com/random/400×400" alt="" />
+                        <span className="seperator"></span>
+                        <div>
+                            <img className="member-img" src="https://source.unsplash.com/random/300×300" alt="" />
+                            <img className="member-img" src="https://source.unsplash.com/random/350×350" alt="" />
+                            <img className="member-img" src="https://source.unsplash.com/random/400×400" alt="" />
+                        </div>
                         <button className="board-filter-btn share-btn full-btn"><ShareSvg /><span>Share</span></button>
                         <button className="board-filter-btn dots"><DotsSvg /></button>
                     </section>
